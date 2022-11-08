aa123d90595f3b209bca9e85e692dbb87a34a105
m4d0x <robert.oxhjarta@hotmail.com>
2022-11-08 15:09:08 +0100

    KeyHandler added into input

--- UserInterface/Input.cs
+++ UserInterface/Input.cs
@@ -2,73 +2,50 @@
 
 public class Input
 {
-    public static ConsoleKeyInfo KeyPress { get; set; }
+    private Dictionary<string, ConsoleKey> _movementInputDict;
 
-    
-    /*
-    public static void KeyActionHandler()
+    public Input()
     {
-        GetKeyInput();
+        _movementInputDict = SetMovementInput();
     }
 
-    public static void GetKeyInput()
+    private Dictionary<string, ConsoleKey> SetMovementInput()
     {
-        ConsoleKeyInfo keyPress = Console.ReadKey();
-        if (keyPress.Key == ConsoleKey.UpArrow || keyPress.Key == ConsoleKey.W)
+        Dictionary<string, ConsoleKey> tempDict = new();
+        tempDict.Add("ArrowUp", ConsoleKey.UpArrow);
+        tempDict.Add("ArrowDown", ConsoleKey.DownArrow);
+        tempDict.Add("ArrowLeft", ConsoleKey.LeftArrow);
+        tempDict.Add("ArrowRight", ConsoleKey.RightArrow);
+        return tempDict;
+    }
+
+    public void CheckIfMovementInput(ConsoleKey consoleKey) // vi hittar
+    {
+        foreach (var item in _movementInputDict)
         {
-            MoveObjectUp();// nuvarande position ska in
-        }
-        else if (keyPress.Key == ConsoleKey.LeftArrow || keyPress.Key == ConsoleKey.A)
-        {
-            MoveObjectLeft();// nuvarande position ska in
-        }
-        else if (keyPress.Key == ConsoleKey.RightArrow || keyPress.Key == ConsoleKey.D)
-        {
-           MoveObjectRight();// nuvarande position ska in
-        }
-        else if
-            (keyPress.Key == ConsoleKey.DownArrow || keyPress.Key == ConsoleKey.S)
-        {
-            MoveObjectDown();// nuvarande position ska in
+            if (item.Value == consoleKey)
+            {
+               HandleMovementInput(consoleKey);
+            }
         }
     }
 
-    public static void MoveObjectUp(int currentX, int currentY)
+    public void HandleMovementInput(ConsoleKey movementKey) // vi hanterar om vi hittar knappen
     {
-        Draw.RenderPlayer(1,1--).y--;
+        switch (movementKey)
+        {
+            case ConsoleKey.UpArrow:
+                //MovementLogicHandler();
+                break;
+            case ConsoleKey.DownArrow:
+                //MovementLogicHandler();
+                break;
+            case ConsoleKey.LeftArrow:
+                //MovementLogicHandler();
+                break;
+            case ConsoleKey.RightArrow:
+                //MovementLogicHandler();
+                break;
+        }
     }
-    public static void MoveObjectLeft(int currentX, int currentY)
-    {
-        Draw.RenderPlayer.x--;
-    }
-    public static void MoveObjectRight(int currentX, int currentY)
-    {
-        Draw.RenderPlayer.x++;
-    }
-    public static void MoveObjectDown(int currentX, int currentY)
-    {
-        Draw.RenderPlayer.y++;
-    }*/
 }
-
-/*public static void ToggleCharacter()
-{
-    Console.SetCursorPosition(29,7);
-    ConsoleKeyInfo keyPress = Console.ReadKey();
-    if (keyPress.Key == ConsoleKey.LeftArrow || keyPress.Key == ConsoleKey.A)
-    {
-        Draw.RenderPlayer.x--;
-    }
-    else if (keyPress.Key == ConsoleKey.RightArrow || keyPress.Key == ConsoleKey.D)
-    {
-        Draw.RenderPlayer.x++;
-    }
-    else if (keyPress.Key == ConsoleKey.UpArrow || keyPress.Key == ConsoleKey.W)
-    {
-        Draw.RenderPlayer.y--;
-    }
-    else if (keyPress.Key == ConsoleKey.DownArrow || keyPress.Key == ConsoleKey.S)
-    {
-        Draw.RenderPlayer.y++;
-    }
-}*/
