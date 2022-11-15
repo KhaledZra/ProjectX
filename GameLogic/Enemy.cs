5dd61144cde7278686fb245292fad307d834866d
Omar <omar.m10@hotmail.com>
2022-11-15 10:26:33 +0100

    enemy list added

--- GameLogic/Enemy.cs
+++ GameLogic/Enemy.cs
@@ -1,6 +1,8 @@
 namespace GameLogic;
 public class Enemy : Entity
 {
+    private List<Enemy> enemies = new();
+   
     public Enemy(string name) : base(name)
     {
         
@@ -12,4 +14,24 @@ public class Enemy : Entity
             { Health = 100 , LevelStats = new LevelHandler()
                 {Experience = 100} };
     }
+
+    public static List<Enemy> GenerateEnemies()
+    {
+        Enemy myEnemies = new("Kel’Thuzad");
+        Enemy myEnemies2 = new("Lich King");
+        Enemy myEnemies3 = new("ARCHIMONDE");
+        Enemy myEnemies4 = new("KIL’JAEDEN");
+        Enemy myEnemies5 = new("DEATHWING");
+
+        List<Enemy> enemyList = new();
+
+        enemyList.Add(myEnemies);
+        enemyList.Add(myEnemies2);
+        enemyList.Add(myEnemies3);
+        enemyList.Add(myEnemies4);
+        enemyList.Add(myEnemies5);
+
+        return enemyList;
+    }
+
 }
