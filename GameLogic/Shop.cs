4e57711f5f8f0ccc5a108fe9ac460d2feb336ead
Khaled Zra <halo_434000@hotmail.com>
2022-11-19 09:35:20 +0100

    Changed the way shop gets list and removed unused code

--- GameLogic/Shop.cs
+++ GameLogic/Shop.cs
@@ -1,60 +1,47 @@
-using System.Net.Mail;
-
 namespace GameLogic;
 
 public class Shop
 {
-    public List<Item> StockInShop { get; set; } = new();
-    public static int Currency { get; set; }
+    public List<Item> StockInShop { get; set; }
 
     public Shop()
     {
-        //_stockInShop = new List<Item>();
-        Currency = 100000000; // Shop starts with 100 gold
-        //SetItemToShopList();
-        GetStockOfShop(StockInShop);
+        StockInShop = DefaultStock(); // Borde skickas från DB
     }
 
-    public override string ToString()
+    private List<Item> DefaultStock() //not supposed to be here, list should be in DB
     {
-        return $"{StockInShop} - Prize: {Currency}";
-    }
-
-    public static List<Item> GetStockOfShop(List<Item> shopItems)
-    {
-        return shopItems.ToList();
-    }
-
-    private void DefaultStock() //not supposed to be here
-    {
+        List<Item> tempList = new List<Item>();
         //distance weapons
-        StockInShop.Add(new Item("Shuriken", 5, 1, 1));
-        StockInShop.Add(new Item("Short Bow", 10, 1, 10));
-        StockInShop.Add(new Item("Batarang", 50, 5, 25));
-        StockInShop.Add(new Item("Long Bow", 20, 5, 30));
-        StockInShop.Add(new Item("Crossbow", 30, 10, 100));
-        StockInShop.Add(new Item("Fūma Shuriken, major throwing star", 25, 30, 150)); //large shurikens
-        StockInShop.Add(new Item("Widow's Wail", 100, 49, 1500));
+        tempList.Add(new Item("Shuriken", 5, 1, 1));
+        tempList.Add(new Item("Short Bow", 10, 1, 10));
+        tempList.Add(new Item("Batarang", 50, 5, 25));
+        tempList.Add(new Item("Long Bow", 20, 5, 30));
+        tempList.Add(new Item("Crossbow", 30, 10, 100));
+        tempList.Add(new Item("Fūma Shuriken, major throwing star", 25, 30, 150)); //large shurikens
+        tempList.Add(new Item("Widow's Wail", 100, 49, 1500));
 
         //end game ranged weapons
-        StockInShop.Add(new Item("The Spirit Spear Chastiefol", 10000, 50, 9999999)); //mythical
-        StockInShop.Add(new Item("Death Note", 10000, 50, 9999999)); //mythical
+        tempList.Add(new Item("The Spirit Spear Chastiefol", 10000, 50, 9999999)); //mythical
+        tempList.Add(new Item("Death Note", 10000, 50, 9999999)); //mythical
 
         //Handwielded weapons
-        StockInShop.Add(new Item("Rusty Sword", 10, 1, 1));
-        StockInShop.Add(new Item("Butcher's Axe", 20, 5, 10));
-        StockInShop.Add(new Item("Jagged Scimitar", 20, 5, 25));
-        StockInShop.Add(new Item("Longsword", 30, 10, 30));
-        StockInShop.Add(new Item("Skullcracker's Mace", 35, 10, 50));
-        StockInShop.Add(new Item("Needle", 50, 30, 15000));
-        StockInShop.Add(new Item("Kusanagi", 100, 49, 30000));
-        StockInShop.Add(new Item("Stonecutter Axe", 100, 49, 90000));
+        tempList.Add(new Item("Rusty Sword", 10, 1, 1));
+        tempList.Add(new Item("Butcher's Axe", 20, 5, 10));
+        tempList.Add(new Item("Jagged Scimitar", 20, 5, 25));
+        tempList.Add(new Item("Longsword", 30, 10, 30));
+        tempList.Add(new Item("Skullcracker's Mace", 35, 10, 50));
+        tempList.Add(new Item("Needle", 50, 30, 15000));
+        tempList.Add(new Item("Kusanagi", 100, 49, 30000));
+        tempList.Add(new Item("Stonecutter Axe", 100, 49, 90000));
 
         //end game handheld weapons    <
-        StockInShop.Add(new Item("Newgate's Murakumogiri", 100, 49, 9999999)); //legendary
-        StockInShop.Add(new Item("The Vorpal Blade", 100, 50, 9999999)); //legendary
-        StockInShop.Add(new Item("Mihawk's Yoru", 1000, 50, 9999999)); //legendary
-        StockInShop.Add(new Item("Orcus, Scythe of the Reaper", 1000, 50, 9999999)); //mythical
-        StockInShop.Add(new Item("The Sword of Rupture, Ea", 10000, 50, 9999999)); //mythical
+        tempList.Add(new Item("Newgate's Murakumogiri", 100, 49, 9999999)); //legendary
+        tempList.Add(new Item("The Vorpal Blade", 100, 50, 9999999)); //legendary
+        tempList.Add(new Item("Mihawk's Yoru", 1000, 50, 9999999)); //legendary
+        tempList.Add(new Item("Orcus, Scythe of the Reaper", 1000, 50, 9999999)); //mythical
+        tempList.Add(new Item("The Sword of Rupture, Ea", 10000, 50, 9999999)); //mythical
+
+        return tempList;
     }
 }
